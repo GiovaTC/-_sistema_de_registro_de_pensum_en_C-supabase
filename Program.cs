639044d4fac319e05b3d2596100d4bb0289d01fd
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-24 16:46:42 -0500

    commit - dos .

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+// Programa completo: Registro de asignaturas y guardado en Supabase
+// Reemplaza TU_URL_SUPABASE y TU_API_KEY en el método GuardarEnSupabase
+
+class Asignatura
+{
+    public string Codigo { get; set; }
+    public string Nombre { get; set; }
+    public string Prerrequisito { get; set; }
+
+    public void ImprimirDiagrama()
+    {
+        Console.WriteLine("\n+--------------------+");
+        Console.WriteLine("|     Asignatura     |");
+        Console.WriteLine("+--------------------+");
+        Console.WriteLine($"| Código: {Codigo}");
+        Console.WriteLine($"| Nombre: {Nombre}");
+        Console.WriteLine($"| Prerreq: {Prerrequisito}");
+        Console.WriteLine("+--------------------+");
+    }
+}
+
+class Profesor
+{
+    public string Nombre { get; set; }
+    public string Especialidad { get; set; }
+    public void ImprimirDiagrama()
+    {
+        Console.WriteLine("\n+--------------------+");
+        Console.WriteLine("|      Profesor      |");
+        Console.WriteLine("+--------------------+");
+        Console.WriteLine($"| Nombre: {Nombre}");
+        Console.WriteLine($"| Departamento: {Especialidad}");
+        Console.WriteLine("+--------------------+");
+    }
+}
