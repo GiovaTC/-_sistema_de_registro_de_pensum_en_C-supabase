637498a2c036a0549a311c077876aa4d3d13393f
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-24 18:02:22 -0500

    commit - final :.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
@@ -67,3 +68,73 @@ class Programa
     }
 }
 
+class Program
+{
+    static async Task Main()
+    {
+        var programa = new Programa { Nombre = "Ingeniería de Sistemas" };
+
+        Console.WriteLine("=== Registro Pensum Sistemas ===");
+        bool continuar = true;
+
+        while (continuar)
+        {
+            var asignatura = RegistrarAsignatura();
+
+            asignatura.ImprimirDiagrama();
+            programa.AgregarAsignatura(asignatura);
+            await GuardarEnSupabase(asignatura);
+
+            Console.Write("¿Desea agregar otra asignatura? (s/n): ");
+            continuar = Console.ReadLine()?.Trim().ToUpper() == "S";
+        }
+
+        programa.ImprimirDiagramaGeneral();
+        Console.WriteLine("Proceso finalizado");
+        Console.WriteLine("Presione ENTER para salir...");
+        Console.ReadLine();
+    }
+
+    static Asignatura RegistrarAsignatura()
+    {
+        Console.Write("\nCódigo asignatura: ");
+        string codigo = Console.ReadLine();
+        Console.Write("Nombre asignatura: ");
+        String nombre = Console.ReadLine();
+        Console.Write("Prerrequisito: ");
+        string prerrequisito = Console.ReadLine();
+
+        return new Asignatura
+        {
+            Codigo = codigo,
+            Nombre = nombre,
+            Prerrequisito = prerrequisito
+        };
+    }
+
+    static async Task GuardarEnSupabase(Asignatura asignatura)
+    {
+        string url = "https://wohhwcvrvwogmfnqrxwy.supabase.co/rest/v1/asignaturas";
+        string apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvaGh3Y3ZydndvZ21mbnFyeHd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NzQzMDcsImV4cCI6MjA3NjU1MDMwN30.d-X1cA3PXaqAS5VnPWsTOLZEO16ajBIsrN_aggr9nQs";
+
+        try
+        {
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("apikey", apikey);
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apikey}");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await client.PostAsJsonAsync(url, asignatura);
+
+            if (response.IsSuccessStatusCode)
+
+                Console.WriteLine("Guardado en Supabase ✔");
+            else
+                Console.WriteLine("Error Supabase ❌: " + await response.Content.ReadAsStringAsync());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Excepción al intentar guardar en Supabase: " + ex.Message);
+        }
+    }
+}
